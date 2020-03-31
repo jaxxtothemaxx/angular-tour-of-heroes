660cff7c1933ae84ccabeaaa004669250d760d39
JaxxtotheMaxx <offbeat5024@gmail.com>
2020-03-31 13:49:15 -0400

    Export MessageService with cache and add/clear methods, inject MessageService into HeroService, create public messageService parameter in HeroService contructor

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(public messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]>{
     return of(HEROES);
