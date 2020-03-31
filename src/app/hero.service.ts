cbc4154bf018328a744d5f1141054043b1e35fb1
JaxxtotheMaxx <offbeat5024@gmail.com>
2020-03-31 13:55:03 -0400

    Modify getHeroes in hero.service to send message when heroes are fetched, inject message service into message component class, declare messageService parameter publicly in message component class constructor

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,8 +10,9 @@ import { MessageService } from './message.service';
 export class HeroService {
 
   constructor(public messageService: MessageService) { }
-
   getHeroes(): Observable<Hero[]>{
+    //TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
