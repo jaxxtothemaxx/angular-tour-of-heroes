47d1845d35a931dccb0f7731b6a8b49748492bd2
JaxxtotheMaxx <offbeat5024@gmail.com>
2020-04-01 13:14:41 -0400

    Added a save button to hero-detail template, created a save method in the hero detail component class to bind to the save button, added an http method to hero service to update a hero name

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,6 +11,10 @@ import { MessageService } from './message.service';
 export class HeroService {
   private heroesUrl = 'api/heroes';
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application-json' })
+  };
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
@@ -48,6 +52,13 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  updateHero( hero: Hero ): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
