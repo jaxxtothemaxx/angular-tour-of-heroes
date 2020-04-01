d687b802fb673bfe17a1d30eee14045129d0fc21
JaxxtotheMaxx <offbeat5024@gmail.com>
2020-04-01 15:11:32 -0400

    Create http search functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -79,6 +79,18 @@ export class HeroService {
     );
   }
 
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`no heroes matching "${term}"`) :
+        this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
